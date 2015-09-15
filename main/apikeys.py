c105d33c407ba15bc4efc5560246a3c7c0540cc8
Eric Turner <EricTurnerNV@gmail.com>
2015-09-15 11:25:11 -0700

    Cleaned up Forms.Py

--- main/apikeys.py
+++ main/apikeys.py
@@ -1,5 +1,6 @@
 import os
 
+# Storage for API Keys
 trailkey = os.environ['trailkey']
 weatherkey = os.environ['weatherkey']
 googlekey = os.environ['googlekey']
--- main/forms.py
+++ main/forms.py
@@ -3,22 +3,23 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 from main.models import Trip
-import datetime
-from django.forms.extras.widgets import SelectDateWidget
-from django.forms import ModelForm, Form
 
-date_field = forms.DateField(widget=SelectDateWidget)
 
 class CamperCreateForm(UserCreationForm):
-    """Form for Registering new Campers"""
+    """
+    Form for registering New Campers
+    """
     zip = forms.CharField(max_length=10, required=True)
     email = forms.EmailField(required=True)
+
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name')
 
 
 class TripCreateForm(ModelForm):
+    """
+    Form for creating a new trip
+    """
     model = Trip
-    fields = ('date')
-
+    fields = ('date',)
